686ca631c1a1061a1930d84c3afcbfa82f5b11ff
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-26 17:40:19 +0600

    task

--- index.js
+++ index.js
@@ -32,9 +32,49 @@ callNamePrice.appendChild(div);
 
 const totalP =document.getElementById('totalPrice');
 store = store + parseInt(price);
-// console.log(store);
-// const sum = parent(totalP) + 
-totalP.innerText =store
+totalP.innerText =store;
 
+
+
+// 
+// // console.log(btn);
+
+//    
+//     const dis = document.getElementById('discountPrice');
+// //   console.log(typeof dis);
+// // const ff = parseInt(dis)
+//             const discount = totalP * 0.2;
+//             console.log(typeof discount);
+//             dis.textContent = discount;
+        
+    
     })
 }
+
+const btn = document.getElementById('apply-btn');
+btn.addEventListener('click',() => {
+    const input =document.getElementById('input-field').value;
+// console.log("clicked");
+const modify =input.split(' ').join('').toUpperCase();
+// console.log(modify);
+if (store > 200) {
+    const dis = document.getElementById('discountPrice');
+    if (modify == "SELL20") {
+        const discount = store * 0.2;
+        const finalPrice =discount.toFixed(2);
+        dis.innerText =finalPrice;
+        // return finalPrice;
+        const totalPrices = store - finalPrice;
+        // console.log(totalPrices);
+        const total = document.getElementById('total');
+        total.innerText = totalPrices;
+    }else{
+        alert("invalid coupon price")
+    }
+}else{
+    alert('minimum purchase price $ 200')
+}
+
+
+
+})
